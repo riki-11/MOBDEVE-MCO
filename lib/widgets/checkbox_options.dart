1e39cd1b49597c751fa901a6238cd905547b0519
PJLeonida <patrick_josh_c_leonida@dlsu.edu.ph>
2024-10-29 01:19:39 +0800

    style: Fixed checkbox width display

--- lib/widgets/checkbox_options.dart
+++ lib/widgets/checkbox_options.dart
@@ -16,7 +16,7 @@ class CheckboxOptions extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
       child: Column(
         children: [
           CheckboxListTile(
