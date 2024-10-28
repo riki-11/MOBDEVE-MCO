f69a24a3dd2e3c1cf8df41f42d4f802108360aa5
PJLeonida <patrick_josh_c_leonida@dlsu.edu.ph>
2024-10-28 19:07:23 +0800

    feat: add create-article page

---
+++ lib/widgets/checkbox_options.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CheckboxOptions extends StatelessWidget {
+  final String option;
+  final bool value;
+  final void Function(bool?) onChange;
+
+  const CheckboxOptions({
+    super.key,
+    required this.option,
+    required this.value,
+    required this.onChange
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Column(
+        children: [
+          CheckboxListTile(
+              title: Text(
+                option,
+                style: Theme.of(context).textTheme.bodyLarge
+              ),
+              value: value,
+              onChanged: onChange),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.875,
+            child: const Divider(),
+          )
+        ],
+      ),
+    );
+  }
+}
