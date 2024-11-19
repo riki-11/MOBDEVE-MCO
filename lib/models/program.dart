f10051a78948019babdab49a5e9e56080f774f08
Cornars <85560422+Cornars@users.noreply.github.com>
2024-11-19 12:11:00 +0800

    feat: Add initial Program controller

--- lib/models/program.dart
+++ lib/models/program.dart
@@ -1,14 +1,25 @@
 import 'college.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Program {
-  String id;
-  String name;
-  String acronym;
-  College college;
+  String? id;
+  late String name;
+  late String acronym;
+  late College college;
 
-  Program(
-      {required this.id,
-      required this.name,
-      required this.acronym,
-      required this.college});
+  Program({
+    required this.id,
+    required this.name,
+    required this.acronym,
+    required this.college,
+  });
+
+  Program.fromDocumentSnapshot({required DocumentSnapshot documentSnapshot}) {
+    id = documentSnapshot.id;
+    name = documentSnapshot['name'];
+    acronym = documentSnapshot['acronym'];
+    college = College.fromDocumentSnapshot(
+      documentSnapshot: documentSnapshot['college'],
+    );
+  }
 }
