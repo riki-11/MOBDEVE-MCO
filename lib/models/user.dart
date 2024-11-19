2ea0b840c8b1ba944956187923bcc6b92fdea9af
Cornars <85560422+Cornars@users.noreply.github.com>
2024-11-19 12:10:34 +0800

    fix: change User to match Cloud Firestore

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,15 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class User {
-  String id;
-  String email;
-  String username;
-  String colleges;
-  String programs;
-  String lists;
+  String? id;
+  late String email;
+  late String firstName;
+  late String lastName;
+  late String colleges;
+  late List<String> programs;
+  late List<String> lists;
+
   User(
       {required this.id,
       required this.email,
-      required this.username,
+      required this.lastName,
+      required this.firstName,
       required this.colleges,
       required this.lists,
       required this.programs});
+
+  User.fromDocumentSnapshot({required DocumentSnapshot documentSnapshot}) {
+    id = documentSnapshot.id;
+    email = documentSnapshot['email'];
+    firstName = documentSnapshot['firstName'];
+    lastName = documentSnapshot['lastName'];
+    colleges = documentSnapshot['colleges'];
+    programs = List<String>.from(documentSnapshot['programs']);
+    lists = List<String>.from(documentSnapshot['lists']);
+  }
 }
