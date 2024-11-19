87da0aba1405d1323f0d192ed56b15d5e9502bad
riki-11 <lejanoenrique@gmail.com>
2024-11-19 17:17:23 +0800

    feat: add share and edit buttons to my-profile page.

--- lib/pages/my-profile.dart
+++ lib/pages/my-profile.dart
@@ -32,15 +32,62 @@ class _MyProfilePageState extends State<MyProfilePage> {
           )
         ],
       ),
-      body: const Column(
+      body: Column(
         children: <Widget>[
-          ProfileHeader(
+          const ProfileHeader(
               username: "Enrique Lejano",
               collegeName: "College of Computer Studies",
               collegeAcronym: "CCS",
               programName: "BSMS Computer Science Major in Software Technology",
               programAcronym: "BSMS-CS"
           ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            margin: const EdgeInsets.symmetric(vertical: 8.0),
+            child: Flex(
+              direction: Axis.horizontal,
+              children: <Widget>[
+                Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: TextButton(
+                          onPressed: () {}, // TODO: Can insert third-party APIs for sharing.
+                          style: TextButton.styleFrom(
+                            backgroundColor: Theme.of(context).colorScheme.primary
+                          ),
+                          child: Text(
+                            "Share",
+                            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                              color: Theme.of(context).colorScheme.onPrimary
+                            )
+                          )
+                      ),
+                    )
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        side: BorderSide(
+                          color: Theme.of(context).dividerColor
+                        )
+                    ),
+                      child: Text(
+                        "Edit",
+                        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          color: Colors.black
+                        )
+                      )
+                    ),
+                  ),
+                ),
+              ],
+            )
+          )
         ],
       ),
       bottomNavigationBar: StandardBottomBar(curPageIndex: pageIndex),
