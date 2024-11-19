8647f51b31fe22e5d442a26c9ca7e450fc3ff7d5
riki-11 <lejanoenrique@gmail.com>
2024-11-20 01:15:18 +0800

    feat: switch filter tags to college and programs.

--- lib/widgets/filter_articles_popup.dart
+++ lib/widgets/filter_articles_popup.dart
@@ -6,44 +6,67 @@ class FilterArticlesPopup extends StatefulWidget {
 
   @override
   State<FilterArticlesPopup> createState() => _FilterArticlesPopupState();
-
 }
 
 class _FilterArticlesPopupState extends State<FilterArticlesPopup> {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.sizeOf(context).height * .25,
       width: double.infinity,
       child: Card(
         elevation: 10,
         color: Theme.of(context).cardColor,
         child: Padding(
           padding: const EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 20),
-                child: Text(
-                  "Programs",
-                  style: Theme.of(context).textTheme.titleMedium,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: Text("College", style: Theme.of(context).textTheme.titleLarge),
                 ),
-              ),
-              MultiSelectContainer(
-                itemsDecoration: MultiSelectDecorations(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).cardColor,
-                    border: Border.all(color: Colors.black12),
-                    borderRadius: BorderRadius.circular(20)
+                MultiSelectContainer(
+                  itemsDecoration: MultiSelectDecorations(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).cardColor,
+                      border: Border.all(color: Colors.black12),
+                      borderRadius: BorderRadius.circular(20)),
+                    selectedDecoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      border: Border.all(color: Theme.of(context).primaryColor),
+                      borderRadius: BorderRadius.circular(20))
                   ),
-                  selectedDecoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    border: Border.all(color: Theme.of(context).primaryColor),
-                    borderRadius: BorderRadius.circular(20)
-                  )
+                  maxSelectableCount: 1,
+                  items: [
+                    MultiSelectCard(value: 'CCS', label: 'CCS'),
+                    MultiSelectCard(value: 'GCOE', label: 'GCOE'),
+                    MultiSelectCard(value: 'CLA', label: 'CLA'),
+                    MultiSelectCard(value: 'COS', label: 'COS'),
+                    MultiSelectCard(value: 'COB', label: 'COB'),
+                    MultiSelectCard(value: 'CLTSOE', label: 'CLTSOE'),
+                    MultiSelectCard(value: 'BAGCED', label: 'BAGCED'),
+                    MultiSelectCard(value: 'TDSOL', label: 'TDSOL'),
+                  ],
+                  onChange: (allSelectedItems, selectedItem) {}
                 ),
+                const SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: Text("Program", style: Theme.of(context).textTheme.titleLarge),
+                ),// Space between sections.
+                // TODO: Dynamically change the list of programs depending on the college
+                MultiSelectContainer(
+                  itemsDecoration: MultiSelectDecorations(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).cardColor,
+                      border: Border.all(color: Colors.black12),
+                      borderRadius: BorderRadius.circular(20)),
+                    selectedDecoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      border: Border.all(color: Theme.of(context).primaryColor),
+                      borderRadius: BorderRadius.circular(20))),
                   items: [
                     MultiSelectCard(value: 'BSCS-ST', label: 'BSCS-ST'),
                     MultiSelectCard(value: 'BS-IT', label: 'BS-IT'),
@@ -51,11 +74,14 @@ class _FilterArticlesPopupState extends State<FilterArticlesPopup> {
                     MultiSelectCard(value: 'BSCS-CSE', label: 'BSCS-CSE'),
                     MultiSelectCard(value: 'BSMS-CS', label: 'BSMS-CS'),
                   ],
-                onChange: (allSelectedItems, selectedItem) {}),
-            ],
+                  onChange: (allSelectedItems, selectedItem) {}
+                ),
+                // TODO: add clear and apply buttons here.
+              ],
+            ),
           ),
         ),
-      ),
+      )
     );
   }
 }
