3ebcf15f8e25d72127466ff21b4290ca6057fdb2
riki-11 <lejanoenrique@gmail.com>
2024-11-20 22:35:31 +0800

    feat: add social media sharing popup to my-profile and view-article pages.

--- lib/pages/view-article.dart
+++ lib/pages/view-article.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 import 'package:mobdeve_mco/widgets/article_bottom_bar.dart';
+import 'package:mobdeve_mco/widgets/social_media_sharing_popup.dart';
 import 'package:mobdeve_mco/widgets/standard_scrollbar.dart';
 import 'package:mobdeve_mco/widgets/disappearing_top_bar.dart';
 
@@ -31,7 +32,16 @@ class _ViewArticleState extends State<ViewArticle> {
     return Scaffold(
       body: DisappearingTopBar(
           actions: [
-            IconButton(onPressed: (){}, icon: const Icon(Icons.ios_share_rounded)),
+            IconButton(
+                onPressed: (){
+                  showModalBottomSheet(
+                    context: context,
+                    builder:  (BuildContext context) {
+                      return const SocialMediaSharingPopup();
+                    }
+                  );
+                },
+                icon: const Icon(Icons.ios_share_rounded)),
             PopupMenuButton<String>(
                 icon: const Icon(Icons.more_horiz),
                 onSelected: (String value) {
