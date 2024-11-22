cdb3fb4ba7584caf189379d7325dd8c5467a7122
Cornars <85560422+Cornars@users.noreply.github.com>
2024-11-22 16:33:27 +0800

    feat: add delete article from list controller func

--- lib/controllers/list_controller.dart
+++ lib/controllers/list_controller.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:mobdeve_mco/constants/global_consts.dart';
 import 'package:mobdeve_mco/controllers/user_controller.dart';
+import 'package:mobdeve_mco/models/article.dart';
 import 'package:mobdeve_mco/models/list.dart';
 import 'package:mobdeve_mco/models/user.dart';
 
@@ -82,8 +83,8 @@ class ListController extends GetxController{
   // Delete List 
   Future<void> deleteListOfUser(ListModel listToDelete) async {
     String? currentUserId = UserController.instance.currentUser.value?.id;
-    if (currentUserId == null){
-      throw Exception("Error creating List, current user is null");
+    if(currentUserId == null){
+      throw Exception("Error deleting List, current user is null");
     }
     final documentRef = firebaseFirestore.collection('users').doc(currentUserId).collection('lists').doc(listToDelete.id);
     
@@ -93,10 +94,40 @@ class ListController extends GetxController{
       throw Exception("Error deleting list, List ID not found");
     }
     await documentRef.delete();
-
   }
   // Delete from List
+  Future<void> deleteArticleFromList(ListModel list, Article article) async {
+    // Given an article, we delete the list from a users List of lists. 
+    String? currentUserId = UserController.instance.currentUser.value?.id;
 
+    // Check if the user is logged in
+    if(currentUserId == null){
+      throw Exception("Error deleting article from list, current user is null");
+    }
+    // Check if this list is owned by the user 
+    if(list.authorId != currentUserId){
+      throw Exception("Error deleting article from list, list is not owned by user");
+    }
+    final documentRef = firebaseFirestore.collection('users').doc(currentUserId).collection('lists').doc(list.id);
+    
+    // Check if document exists first, if not throw an error:
+    DocumentSnapshot documentSnapshot = await documentRef.get();
+    if(!documentSnapshot.exists){
+      throw Exception("Error deleting article from list, List ID not found");
+    }
+    // This is the collection
+    ListModel listInCollection = ListModel.fromDocumentSnapshot(documentSnapshot: documentSnapshot);
+
+    // If article was in the list, and was removed
+    if(listInCollection.articlesBookmarked.remove(article.id)){
+      await documentRef.set({
+        'articlesBookmarked': listInCollection.articlesBookmarked,
+      });
+    } else {
+      throw Exception("Error deleting article from list, article not found");
+    }
+    
+  }
   // Add Article to List
   @override
   void onReady() {
