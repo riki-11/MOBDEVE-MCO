58aa7d105da7d08e18bf56652227a25cd2abd57c
Cornars <85560422+Cornars@users.noreply.github.com>
2024-11-24 12:25:02 +0800

    feat: show lists in article view

--- lib/widgets/add_article_to_list.dart
+++ lib/widgets/add_article_to_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_multi_select_items/flutter_multi_select_items.dart';
+import 'package:mobdeve_mco/controllers/list_controller.dart';
+import 'package:mobdeve_mco/models/list.dart';
 
 class AddArticleToList extends StatefulWidget {
   final String articleId;
@@ -12,19 +14,18 @@ class AddArticleToList extends StatefulWidget {
 class _AddArticleToListState extends State<AddArticleToList> {
   late String articleId;
 
-  final MultiSelectController<String?> _controller = MultiSelectController();
+  final MultiSelectController<ListModel> _controller = MultiSelectController();
 
-  // TODO: Insert lists here.
-  final Map<String, String> listOptions = {
-    "1": "List 1: Freshman Year",
-    "2": "List 2: Sophomore Year",
-    "3": "List 3: Junior Year",
-  };
+
+  // Key: List object, Value: List Name
+  late Map<ListModel, String> listOptions;
 
   @override
   void initState() {
     super.initState();
     articleId = widget.articleId;
+    listOptions = {for(ListModel list in ListController.instance.currentUserLists.value)
+      list: list.title};
   }
 
   @override
@@ -78,8 +79,10 @@ class _AddArticleToListState extends State<AddArticleToList> {
             style: ButtonStyle(
               backgroundColor: WidgetStateProperty.all(Theme.of(context).colorScheme.primary),
             ),
-            onPressed: () {
+            onPressed: ()  async {
               // TODO: Save to all the articles.
+              // Lists that were previously selected that were deselected will be removed from the array
+              // Lists that were selected
             },
             child: Text(
               "Confirm",
